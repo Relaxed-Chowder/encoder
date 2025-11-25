dffd1ab0e3f5f2a540e6860021da6ac65c6f28ec
Relaxed-Chowder <avill390@evit.edu>
2025-11-25 14:10:37 -0700

    Update front.java

--- cod/front.java
+++ cod/front.java
@@ -1,33 +1,46 @@
 class front{
     public static void main(String[] args) {
         int encrypt = 1;
-        String[] input = {"46","6F","72","67","6F","74","74","65"};
+        String[] input = {"4L", "6F", "73", "74"};
         int turns = 3;
 
         if(encrypt == 1){
-            //String[] encrypted = scytaleEncrypt(input, turns);
-            double encrypted = scytaleEncrypt(input, turns);
-            System.out.println(encrypted);
-            //for (String s : encrypted) System.out.println(s);
+            String[] encrypted = scytaleEncrypt(input, turns);
+            for (String s : encrypted){ 
+                System.out.println(s);
+            }
         }
     }
 
-    public static double scytaleEncrypt(String[] input, int turns) {
-        int columns = (input.length + 1) / turns;
+    public static String[] scytaleEncrypt(String[] input, int turns) {
+        int columns = (int) Math.ceil((double) input.length / turns);
         String[][] grid;
         grid = new String[turns][columns];
 
         int index = 0;
 
-        for(int i = 0; i<=turns; i++){
-            for(int j = 0; j<=columns; j++){
-                if(j < input.length){
+        for(int i = 0; i<turns; i++){
+            for(int j = 0; j<columns; j++){
+                if(index < input.length){
                     grid[i][j] = input[index];
                 }else{
-                    grid[i][j] = input[index];
+                    grid[i][j] = "00";
                 }
+            index++;
             }
         }
-        return columns;
+
+
+        String[] ciphertext;
+        int cipheradd = 0;
+        ciphertext = new String[turns*columns];
+
+        for(int j = 0; j<columns; j++){
+            for(int i = 0; i<turns; i++){
+                ciphertext[cipheradd] = grid[i][j];
+                cipheradd++;
+            }
+        }
+        return ciphertext;
     }
 }
